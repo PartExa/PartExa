844e410f7b357ac110c61d58d183b59d848b7631
Sebastian Fuchs <sebastian.fuchs@tum.de>
2021-12-09 00:25:18 +0100

    Query and print PartExa and deal.II git version
    - configure a header file that contains the obtained git version
      information (hash and branch)
    - implement functions to print PartExa and deal.II git information to
      the output stream

--- include/partexa/base/print_infos.h
+++ include/partexa/base/print_infos.h
@@ -22,7 +22,10 @@
 #ifndef partexa_base_print_infos_h
 #define partexa_base_print_infos_h
 
+#include <partexa/base/revision.h>
+
 #include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/revision.h>
 
 namespace PartExa
 {
@@ -41,6 +44,30 @@ namespace PartExa
     pcout << "                                                                                " << std::endl;
     pcout << "                      A Particle Library for the Exa-Scale                      " << std::endl;
     pcout << "________________________________________________________________________________" << std::endl;
+    pcout << std::endl;
+    // clang-format on
+  }
+
+  //! print PartExa version
+  inline void
+  print_partexa_version(dealii::ConditionalOStream const &pcout)
+  {
+    // clang-format off
+    pcout << std::endl;
+    pcout << "PartExa branch: " PARTEXA_GIT_BRANCH << std::endl;
+    pcout << "PartExa hash:   " PARTEXA_GIT_REVISION << std::endl;
+    pcout << std::endl;
+    // clang-format on
+  }
+
+  //! print deal.II version
+  inline void
+  print_dealii_version(dealii::ConditionalOStream const &pcout)
+  {
+    // clang-format off
+    pcout << std::endl;
+    pcout << "deal.II branch: " DEAL_II_GIT_BRANCH << std::endl;
+    pcout << "deal.II hash:   " DEAL_II_GIT_REVISION << std::endl;
     pcout << std::endl;
     // clang-format on
   }
