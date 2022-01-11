21b0b3acd3076831f78681921f200bc2160e0bb0
Sebastian Fuchs <sebastian.fuchs@tum.de>
2022-01-11 15:29:05 +0100

    Adapt license header for checks

--- tests/tests.h
+++ tests/tests.h
@@ -1,15 +1,28 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2004 - 2021 by the deal.II authors
+// PartExa - A Particle Library for the Exa-Scale
 //
-// This file is part of the deal.II library.
+// Copyright (C) 2021-2022 by the PartExa authors
 //
-// The deal.II library is free software; you can use it, redistribute
-// it, and/or modify it under the terms of the GNU Lesser General
-// Public License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-// The full text of the license can be found in the file LICENSE.md at
-// the top level directory of deal.II.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// ---------------------------------------------------------------------
+
+// ---------------------------------------------------------------------
+//
+// This file is adopted from the deal.II library.
+// https://github.com/dealii/dealii/blob/master/tests/tests.h
 //
 // ---------------------------------------------------------------------
 
